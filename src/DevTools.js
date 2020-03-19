bd196d722abb981ac72216ff31a2bb7b9e5f1573
493224687 <caobinhen6@gmail.com>
2020-03-19 16:20:58 +0800

    修改一下

--- src/DevTools.js
+++ src/DevTools.js
@@ -4,7 +4,7 @@ import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
 export default createDevTools(
-    <DockMonitor toggleVisibilityKey='ctrl-h' defaultIsVisible={true}
+    <DockMonitor toggleVisibilityKey='ctrl-h' defaultIsVisible={false}
                  changePositionKey='ctrl-j'>
         <LogMonitor/>
     </DockMonitor>
